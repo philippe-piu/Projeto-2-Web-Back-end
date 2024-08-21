e25040f1d1a5bd8c612e84dfe9531d6a760e82b6
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2024-08-21 11:13:15 -0300

    Criação do administrador apagando usuarios não administradores
    Criação da rota delete usuario adm apaga usuario não adm

--- Controller/administrador.js
+++ Controller/administrador.js
@@ -6,6 +6,8 @@ const Auth = require('../helpers/Auth');
 
 // Rota para criar um novo usuário administrador
 router.post('/create-adm', Auth.autenticarToken, async (req, res) => {
+  
+
   // Extrai as informações do corpo da requisição
   const { nome, email, senha } = req.body;
   
@@ -41,5 +43,38 @@ router.post('/create-adm', Auth.autenticarToken, async (req, res) => {
     res.status(500).json({ msg: 'Erro interno do servidor' });
   }
 });
+
+// Rota para excluir um usuário não administrador
+router.delete('/delete-user/:id', Auth.autenticarToken, async (req, res) => {
+  //pega o id na url
+  const { id } = req.params;
+
+  try {
+    // Busca o usuário no banco de dados pelo ID
+    const usuario = await Usuario.findOne({ where: { id } });
+
+    // Verifica se o usuário existe
+    if (!usuario) {
+      return res.status(404).json({ msg: 'Usuário não encontrado' });
+    }
+
+    // Verifica se o usuário é um administrador
+    if (usuario.admin) {
+      return res.status(403).json({ msg: 'Não é possível excluir administradores' });
+    }
+
+    // Exclui o usuário do banco de dados
+    await Usuario.destroy({ where: { id } });
+
+    // Retorna uma mensagem de sucesso
+    res.status(200).json({ msg: 'Usuário excluído com sucesso' });
+    console.log("Usuário excluído com sucesso")
+
+  } catch (error) {
+    console.error('Erro ao excluir usuário:', error);
+    res.status(500).json({ msg: 'Erro interno do servidor' });
+  }
+});
+
 
 module.exports = router;
