bda0e0209daf0afc3a256f4f5e9c7e5becdf4061
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2024-08-21 10:45:41 -0300

    Auteração e comentarios
    Auteração e comentarios na Auth.js e administrador.js.

--- Controller/administrador.js
+++ Controller/administrador.js
@@ -6,19 +6,26 @@ const Auth = require('../helpers/Auth');
 
 // Rota para criar um novo usuário administrador
 router.post('/create-adm', Auth.autenticarToken, async (req, res) => {
+  // Extrai as informações do corpo da requisição
   const { nome, email, senha } = req.body;
   
+    // Verifica se todos os campos obrigatórios foram fornecidos
   if (!nome || !email || !senha) {
+    //Retorna erro se algum campo não foi preenchido
     return res.status(400).json({ msg: 'Todos os campos são obrigatórios' });
   }
   
   try {
+    //Verifica se existe o email fornecido
     const usuarioExistente = await Usuario.findOne({ where: { email } });
+
     if (usuarioExistente) {
       return res.status(400).json({ msg: 'Email já cadastrado' });
     }
-    
+    //pega a senha junto com o bcrypt meio que esconde ela no banco
     const hashedSenha = await bcrypt.hash(senha, 10);
+
+    //Cria um novo usuário administrador no banco/tabela Usuario
     const novoUsuario = await Usuario.create({
       nome,
       email,
@@ -26,9 +33,11 @@ router.post('/create-adm', Auth.autenticarToken, async (req, res) => {
       admin: true
     });
     
+    //Manda uma mensagem de sucesso na criação de um adm
     res.status(201).json({ msg: 'Novo administrador criado com sucesso', usuario: novoUsuario });
   } catch (error) {
     console.error('Erro ao criar novo administrador:', error);
+    //Em caso de erro manda uma mensagem para erro
     res.status(500).json({ msg: 'Erro interno do servidor' });
   }
 });
--- helpers/Auth.js
+++ helpers/Auth.js
@@ -1,12 +1,25 @@
 const jwt = require('jsonwebtoken');
 
 const autenticarToken = (req, res, next) => {
+
+  //Pega o token gerado no login que vai ser usado bo barerToken ódigo divide a string pelo espaço para obter o token em si (split(' ')[1])
   const token = req.headers['authorization']?.split(' ')[1];
   
-  if (token == null) return res.sendStatus(401);
-  
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+  //Se o o token do administrador não for encontrado
+  if (token == null){
+    //retorn uma mensagem de erro
+    return res.sendStatus(401).json({ msg: 'Token Invalido usuario não é adm' })
+  } 
+
+  console.log("Token-- ",token);
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    //Se over erro na verificação do token
+    if (error){
+      //retorn uma mensagem de erro
+      return res.sendStatus(403).json({ msg: 'Token Invalido verifique' });
+    } 
+     // Se o token for válido, as informações do usuário são anexadas ao req e a requisição continua para o próximo middleware
     req.user = user;
     next();
   });
