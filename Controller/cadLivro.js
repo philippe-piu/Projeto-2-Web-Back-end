2b235e8ca608a3d54bb4617c6ff410a3063dd715
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2024-08-26 15:21:06 -0300

    Delete de Livros
    Feito o delete dos Livros no Banco de dados

--- Controller/cadLivro.js
+++ Controller/cadLivro.js
@@ -16,22 +16,26 @@ router.post('/create-Livro', Auth.autenticarToken, async (req, res) => {
   }
 
   // Obtém o ID do usuário a partir do token de autenticação
-  const usuarioId = req.user.id;
-  
+  const usuarioId = req.user.id
+
   try {
     // Encontrar o usuário no banco de dados pelo ID
-    const usuario = await Usuario.findOne({ where: { id: usuarioId } });
+    const usuario = await Usuario.findOne({ where: { id: usuarioId } })
     //Se o o Usuario não for um administrador ele não cadastra o livro
-    if(!usuario.admin){
-      return res.status(400).json({ msg: 'Usuário não é administrador você não tem autorização para cadastrar livros'})
+    if (!usuario.admin) {
+      return res
+        .status(400)
+        .json({
+          msg: 'Usuário não é administrador você não tem autorização para cadastrar livros'
+        })
     }
 
     // Verifica se o livro já está cadastrado
-    const livroExistente = await Livro.findOne({ where: { nomeLivro } });
+    const livroExistente = await Livro.findOne({ where: { nomeLivro } })
     //Se o livro estiver Cadastrado ele informa que o livro já está cadatrado
     if (livroExistente) {
-      console.log('Livro já cadastrado');
-      return res.status(400).json({ msg: "Livro já cadastrado" });
+      console.log('Livro já cadastrado')
+      return res.status(400).json({ msg: 'Livro já cadastrado' })
     }
 
     //Criação de informações na tabela
@@ -50,6 +54,45 @@ router.post('/create-Livro', Auth.autenticarToken, async (req, res) => {
   } catch (error) {
     console.error('Erro ao cadastrar livro:', error)
     //Em caso de erro manda uma mensagem para erro
+    res.status(500).json({ msg: 'Erro interno do servidor' })
+  }
+})
+
+router.delete('/delete-livro/:id', Auth.autenticarToken, async (req, res) => {
+  //pega o id na url ndo livro
+  const { id } = req.params
+
+  // Obtém o ID do usuário a partir do token de autenticação
+  const usuarioId = req.user.id
+
+  try {
+    // Encontrar o usuário no banco de dados pelo ID
+    const usuario = await Usuario.findOne({ where: { id: usuarioId } })
+    //Se o o Usuario não for um administrador ele não cadastra o livro
+    if (!usuario.admin) {
+      return res
+        .status(400)
+        .json({
+          msg: 'Usuário não é administrador você não tem autorização para cadastrar livros'
+        })
+    }
+
+    // Busca o usuário no banco de dados pelo ID
+    const livro = await Livro.findOne({ where: { id } })
+
+    // Verifica se o usuário existe
+    if (!livro) {
+      return res.status(404).json({ msg: 'Livro não encontrado' })
+    }
+
+    // Exclui o usuário do banco de dados
+    await Livro.destroy({ where: { id } })
+
+    // Retorna uma mensagem de sucesso
+    res.status(200).json({ msg: 'Livro excluído com sucesso' })
+    console.log('Livro excluído com sucesso')
+  } catch (erro) {
+    console.error('Erro ao excluir Livro', error)
     res.status(500).json({ msg: 'Erro interno do servidor' })
   }
 })
